a63ff198d6ece056cc4dc7984b73f824687ec3b0
sarora2200 <sarora2200@gmail.com>
2024-06-22 14:47:36 +0300

    done

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Join from './pages/Join';
+import Contact from './pages/Contact'; // Corrected import
 import PetList from './components/PetList';
 import OurCustomers from './pages/OurCustomers'; // Corrected import
 
@@ -17,6 +18,7 @@ function App() {
                 <Route path="/about" element={<About />} />
                 <Route path="/join" element={<Join />} />
                 <Route path="/pets" element={<PetList />} />
+                <Route path="/contact" element={<Contact />} />
                 <Route path="/customers" element={<OurCustomers />} />
             </Routes>
         </Router>
