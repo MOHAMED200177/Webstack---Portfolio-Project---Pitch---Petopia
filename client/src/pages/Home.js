d08dad5dac01f1ec00ca242795ce2c93b4b40f92
sarora2200 <sarora2200@gmail.com>
2024-06-26 16:56:55 +0300

    done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,9 +4,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
+import image from './background.png'; // Correctly import the image with its file extension
+
 const Home = () => {
     return (
-        <section id="home">
+        <section id="home" style={{ backgroundImage: `url(${image})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
             <h2>Welcome to Petopia</h2>
             <p>Your one-stop destination for adopting a loving pet.</p>
             <Link to="/pets" className="adopt-button">Adopt Now</Link>
