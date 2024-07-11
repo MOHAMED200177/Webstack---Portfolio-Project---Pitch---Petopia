6053689359ea94bb903ec6a5f0696a709a3f1cda
unknown <mohamedelafandy593@gmail.com>
2024-07-11 03:18:18 -0700

    update pass

--- routes/CustomerRoute.js
+++ routes/CustomerRoute.js
@@ -5,16 +5,20 @@ const router = express.Router();
 
 router.post('/signup', customerController.signup);
 router.post('/login', customerController.login);
+// router.get('/logout', customerController.logout);
+
+router.post('/forgotPassword', customerController.forgotPassword);
+router.patch('/resetPassword/:token', customerController.resetPassword);
 
 router
     .route('/')
-    .get(customerController.protect, customerController.getAllCustomers)
+    .get(customerController.protect, customerController.getAllCustomers);
 // .post(customerController.createCustomer);
 
 router
     .route('/:id')
-    .get(customerController.getCustomer)
-    .patch(customerController.updateCustomer)
-    .delete(customerController.deleteCustomer);
+    .get(customerController.protect, customerController.getCustomer)
+    .patch(customerController.protect, customerController.updateCustomer)
+    .delete(customerController.protect, customerController.restrictTo('admin'), customerController.deleteCustomer);
 
 module.exports = router;
